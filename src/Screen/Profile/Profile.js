36d039e7cceb3ed5e2cc855432cec7a5918b39e8
vitooficial <m.elvin@live.com>
2021-07-10 20:39:52 -0600

    first two

--- src/Screen/Profile/Profile.js
+++ src/Screen/Profile/Profile.js
@@ -1,70 +1,69 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { View, Text, StyleSheet, StatusBar } from 'react-native'
-// import { Icon, Avatar, Input } from "react-native-elements"
-// import { cargarImagenesxAspecto } from '../../Utils/Utils'
+import { Icon, Avatar, Input } from "react-native-elements"
+import { cargarImagenesxAspecto } from '../../Utils/Utils'
 export default function Profile() {
     return (
         <View>
-            {/* <StatusBar backgroundColor='#128c7e' /> */}
-            {/* <CabeceraBG /> */}
-            {/* <HeaderAvatar /> */}
-            <Text> PErf</Text>
+            <StatusBar backgroundColor='#128c7e' />
+            <CabeceraBG />
+            <HeaderAvatar />
 
         </View>
     )
 }
 
-// function HeaderAvatar() {
-//     const cambiarFoto = async () => {
+function HeaderAvatar() {
+    const cambiarFoto = async () => {
 
-//         const resultado = await cargarImagenesxAspecto([1,1])
-//         console.log(resultado);
-//     }
+        const resultado = await cargarImagenesxAspecto([1,1])
+        console.log(resultado);
+    }
 
-//     return (<View style={styles.AvatarLine}>
-//         <Avatar
-//             style={styles.avatar}
-//             source={require("../../../assets/avatar.jpg")}
-//             size="large"
-//             rounded
-//             showAccessory={true}
-//             onAccessoryPress={cambiarFoto}
-//         />
-//     </View>
-//     )
-// }
+    return (<View style={styles.AvatarLine}>
+        <Avatar
+            style={styles.avatar}
+            source={require("../../../assets/avatar.jpg")}
+            size="large"
+            rounded
+            showAccessory={true}
+            onAccessoryPress={cambiarFoto}
+        />
+    </View>
+    )
+}
 
-// function CabeceraBG() {
-//     return (
-//         <View>
+function CabeceraBG() {
+    return (
+        <View>
 
-//             <View style={styles.bg}>
-//                 <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
-//                     Nombre
-//                 </Text>
+            <View style={styles.bg}>
+                <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
+                    Nombre
+                </Text>
 
-//             </View>
-//         </View>
-//     )
-// }
-// const styles = StyleSheet.create({
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
 
-//     bg: {
-//         width: '100%',
-//         height: 200,
-//         borderBottomLeftRadius: 200,
-//         borderBottomRightRadius: 200,
-//         backgroundColor: '#128c7e',
-//         justifyContent: 'center',
-//         alignItems: 'center',
+    bg: {
+        width: '100%',
+        height: 200,
+        borderBottomLeftRadius: 200,
+        borderBottomRightRadius: 200,
+        backgroundColor: '#128c7e',
+        justifyContent: 'center',
+        alignItems: 'center',
 
-//     }, AvatarLine: {
-//         flexDirection: 'row',
-//         justifyContent: 'space-around',
-//         marginTop: -70,
+    }, AvatarLine: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: -70,
 
-//     }, avatar: {
-//         width: 80,
-//         height: 80
-//     }
-// })
+    }, avatar: {
+        width: 80,
+        height: 80
+    }
+})
